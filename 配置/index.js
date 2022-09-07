f2688e4d979fea0ef89bda2ac63ac5fcd1457cc7
lqqqj0421 <112995219+lqqqj0421@users.noreply.github.com>
2022-09-07 10:36:50 +0800

    Update index.js

--- 配置/index.js
+++ 配置/index.js
@@ -31,15 +31,15 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "吃鱼刺的小猫.",
+      name: ",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oUPhc5rcKaeKlc42f0FvKCZTCnps",
+      id: "",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "b3APWaBjWRF3ebj9PZDv2lNcAIwUs1WPoE1zzTMWvfk",
+      useTemplateId: "",
       // 所在省份
-      province: "山西",
+      province: "",
       // 所在城市
-      city: "阳泉",
+      city: "",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
